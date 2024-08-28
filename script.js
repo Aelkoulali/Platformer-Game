d5cd61b202097c4a70b672d1e63c9b16ae1c197e
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-28 08:03:18 +0200

    Set up the ability to add 2D graphics

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+
+// Set up the ability to add 2D graphics
+const ctx = canvas.getContext("2d");
