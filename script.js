fb5e53d522308410947205367fb2f43b67cba218
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-02 08:29:57 +0200

    Add draw( ) to checkpoints class

--- script.js
+++ script.js
@@ -80,6 +80,11 @@ class checkpoints {
         this.position = {x, y};
         this.width = proportionalSize(40);
         this.height = proportionalSize(70);
+        this.claimed = false;
+    }
+    draw(){
+        ctx.fillStyle = "#f1be32";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
 
